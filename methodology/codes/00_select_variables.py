7f3a60d1a89e4098a4230045a7459b4d52b45156
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-10-01 22:28:31 -0400

    feat: data dictionary

--- methodology/codes/00_select_variables.py
+++ methodology/codes/00_select_variables.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-datafile = "N:\Transfer\KSkvoretz\AHRQ\data\\full_data.csv"
+datafile = "data/full_data.csv"
 
 data = pd.read_csv(datafile)
 print(data.shape)
@@ -35,9 +35,40 @@ print(len(keep_vars))
 
 # export all column names to a data dictionary with the following format:
 # column_name|description|demographic|sdoh_raw|outcome|sdoh_score|data_type|used_sdoh_score_1|used_sdoh_score_2|used_sdoh_score_3|used_sdoh_score_4|used_sdoh_score_5|used_sdoh_score_6
+pre_dict = pd.read_csv('data/columns.csv')
 data_dictionary = pd.DataFrame({'column_name': keep_vars,
+# will change this later
 'description': keep_vars})
+
+data_dictionary = pre_dict.merge(data_dictionary, on ='column_name', how = 'inner')
+data_dictionary['demographic'] = [int(dtype == "01_Demographic") for dtype in data_dictionary['type']]
+data_dictionary['sdoh_raw'] = [int(dtype == "02_SDoH") for dtype in data_dictionary['type']]
+data_dictionary['outcome'] = [int(dtype == "03_Outcome") for dtype in data_dictionary['type']]
+data_dictionary.drop('type', axis = 1, inplace = True)
+data_dictionary['sdoh_score'] = 0
+
+def data_type(c):
+    if c in ['FIPS','County','State']:
+        return 'ID'
+    elif data[c].dtypes in ['float64','int64']:
+        if all(data[c] <= 1) or (('%' in c and '95%' not in c) or (c.count("%") > 1)):
+            return 'percentage'
+        else:
+            return 'continuous'
+    elif 'Ratio' in c:
+        return 'ratio'
+    else:
+        return 'unknown'
+
+data_dictionary['data_type'] = data_dictionary['column_name'].apply(data_type)
+# remove rows with unknown
+data_dictionary = data_dictionary[data_dictionary['data_type'] != "unknown"]
+
+for i in range(6):
+    data_dictionary[f'used_sdoh_{i+1}'] = 0
+
 print(data_dictionary)
+data_dictionary.to_csv('data/data_dictionary.csv')
 
 # PCA on variables?
 # which variables indicate the most variation in outcomes?
