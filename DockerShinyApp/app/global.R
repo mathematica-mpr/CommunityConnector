94c9ce0c4a7950f7e24cd56d23bc5a458186b754
Margaret Luo <MLuo@mathematica-mpr.com>
2019-10-08 16:04:43 -0400

    feat: add packages, use config

---
+++ DockerShinyApp/app/global.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(purrr)
+library(tibble)
+library(stringr)
+library(forcats)
+library(shinyWidgets)
+library(yaml)
+
+config <- yaml.load_file("./config.yaml")
