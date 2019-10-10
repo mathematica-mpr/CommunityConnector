ea932d82d8a313b73cf9d446edb3e2e722c6ee4c
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-10-09 20:40:58 -0400

    feat: basic rf

--- methodology/codes/03_model.R
+++ methodology/codes/03_model.R
@@ -12,6 +12,8 @@ data <- read.csv('../../data/data_sdoh_scores.csv')
 # remove variables that have all NAs
 data <- data[,colSums(is.na(data))<nrow(data)]
 # replace NAs with the mean since model won't work without
+# TODO: impute missing values in predictor data using proximity from randomForest
+# rfImpute()
 data <- data.frame(lapply(data, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)))
 na_count <-sapply(data, function(y) sum(length(which(is.na(y)))))
 na_count[na_count > 0]
@@ -24,9 +26,24 @@ trControl <- trainControl(method = 'cv',
 
 set.seed(1234)
 
-rf_default <- train(X..Obese~.,
-                    data = data,
-                    method = 'rf',
-                    metric = 'Accuracy',
-                    trControl = trControl)
-table(is.na(data$X..Obese), exclude = NULL)
+# TODO: lots more parameters
+rf <- randomForest(X..Obese~.,
+                   data = data,
+                   method = 'rf',
+                   metric = 'RMSE',
+                   trControl = trControl, 
+                   keep.forest = TRUE,
+                   importance = TRUE)
+rf
+getTree(rf)
+importance(rf)
+varImp(rf)
+varImpPlot(rf, n.var=10)
+varUsed(rf, by.tree = FALSE, count = TRUE)
+varUsed(rf, by.tree = TRUE, count = TRUE)
+varUsed(rf, by.tree = FALSE, count = FALSE)
+
+# Random forest cross-validation for feature selection
+# rfcv
+
+# tuneRF for the optimal mtry parameter
