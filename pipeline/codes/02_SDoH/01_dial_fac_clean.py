0f8d220f2c176e8b0ca1d62965f0e5a8b8192dfb
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-10-12 19:07:11 -0400

    feat: incorporate dialysis facility data

--- pipeline/codes/02_SDoH/01_dial_fac_clean.py
+++ pipeline/codes/02_SDoH/01_dial_fac_clean.py
@@ -1,4 +1,7 @@
 import pandas as pd 
+import sys
+sys.path.insert(1, 'pipeline/codes')
+from utilities import pull_population
 
 data = pd.read_csv('data/raw/dial_fac_data.csv')
 
@@ -9,6 +12,14 @@ mapping = mapping[['county','FIPS']].drop_duplicates()
 
 print(data.shape)
 data = pd.merge(data, mapping, on = 'county', how = 'left')
+
+pop = pull_population()
+print(pop)
+
+data = pd.merge(data, pop, on = 'FIPS', how = 'left')
+data['dial_facil_pp_rate'] = data['num_dial_facil']/data['population']
+data.drop(['population','county','num_dial_facil'], axis = 1, inplace = True)
+
 print(data.shape)
 print(data)
 
