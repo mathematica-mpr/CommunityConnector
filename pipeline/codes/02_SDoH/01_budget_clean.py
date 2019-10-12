0bd7eb52b677885833af84ef88949345620340d4
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-10-11 21:34:10 -0400

    fix: make budgets per person

--- pipeline/codes/02_SDoH/01_budget_clean.py
+++ pipeline/codes/02_SDoH/01_budget_clean.py
@@ -18,13 +18,24 @@ mapping = mapping[['FIPS','County']]
 
 data = pd.merge(raw_data, mapping, on = 'County', how = 'inner')
 data.drop(['County'], axis = 1, inplace = True)
-assert(raw_data.shape[0] == data.shape[0])
+rows = data.shape[0]
+assert(raw_data.shape[0] == rows)
+
+# make all budget amounts per capita
+pop = pd.read_csv('data/cleaned/01_Demographic/RWJF_cleaned.csv')
+pop = pop[['FIPS','Population_x']]
+data = pd.merge(data, pop, on = "FIPS", how = 'inner')
+data['Sum of Amount'] = data['Sum of Amount']/data['Population_x']
+data.drop(['Population_x'], axis = 1, inplace = True)
+assert(data.shape[0] == rows)
 
 # pivot data from long to wide
 print(data['Division Name'].drop_duplicates())
 # shorten division first
 abbrevs = ['administrative','air','disease','emergency','environmental','waste',
 'health_svcs','laboratory_svcs','health_equity','prevention','planning','water','health_info']
+# add budget prefix to all new column names
+abbrevs = ['budget_' + a for a in abbrevs]
 
 data = pd.pivot_table(data, index = 'FIPS', columns = 'Division Name', values = 'Sum of Amount',
  aggfunc = np.sum)
