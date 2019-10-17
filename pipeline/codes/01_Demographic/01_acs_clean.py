32d8bda5fba11434b6a0e99af9b6ab95eaddb0c3
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-10-17 09:47:36 -0400

    refactor: location to FIPS column

--- pipeline/codes/01_Demographic/01_acs_clean.py
+++ pipeline/codes/01_Demographic/01_acs_clean.py
@@ -4,11 +4,5 @@ import os
 n_drive = "N:/Transfer/KSkvoretz/AHRQ/data/01_Demographic/cleaned"
 
 acs = pd.read_csv(os.path.join(n_drive, "ACS_cleaned.csv"))
-acs.columns.values[0] = "location"
 
-acs['FIPS'] = [location[-3:] for location in acs['location']]
-print(acs['FIPS'])
-
-acs.drop(columns = "location", inplace = True)
-
-acs.to_csv(os.path.join(n_drive, "ACS_cleaned.csv"))
+# columns to keep
