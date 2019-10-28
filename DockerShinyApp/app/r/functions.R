a2281a54960a0be3bd79ced682fbe5a5050c5ed0
Margaret Luo <MLuo@mathematica-mpr.com>
2019-10-28 09:17:51 -0400

    function to pull specific column names from data dictionary; FIPS is now fips in data

--- DockerShinyApp/app/r/functions.R
+++ DockerShinyApp/app/r/functions.R
@@ -1,21 +1,28 @@
+# get specific column names from data dictionary -------------------------------
+get_dd <- function(dd, column_type) {
+  # column type can be: demographic, outcome, or sdoh_score
+  dd %>% filter(eval(parse(text = column_type)) == 1) %>% 
+    select(column_name, description, descrip_new)
+}
+
 # distance function to find my county matches
 
 find_my_matches <- function(my_county, df, n_matches = 20) {
-  # my_county = FIPs of selected county
-  df_score <- df %>% select(FIPS, starts_with("sdoh_score")) %>%
-    column_to_rownames(var = "FIPS")
+  # my_county = fips of selected county
+  df_score <- df %>% select(fips, starts_with("sdoh_score")) %>%
+    column_to_rownames(var = "fips")
   distances <- data.frame(as.matrix(dist(df_score)))
   
   my_county_distances <- distances %>% 
     select(distance = paste0("X", my_county)) %>%
-    rownames_to_column("FIPS") %>%
-    left_join(df %>% select(FIPS, County), by = "FIPS")
+    rownames_to_column("fips") %>%
+    left_join(df %>% select(fips, County), by = "fips")
   
   my_matches <-  my_county_distances %>%
-    filter(FIPS != my_county) %>%
+    filter(fips != my_county) %>%
     arrange(distance) %>%
     head(n_matches) %>%
-    pull(FIPS)
+    pull(fips)
   
   list(my_county_distances, my_matches)
   
@@ -32,6 +39,7 @@ make_radar_data <- function(county_df, dd) {
     rename_at(vars(dd$column_name), ~ dd$descrip_new)
   df
 }
+
 
 make_density_graph <- function(data) {
   ggplot(data, aes(x=value)) + geom_density() + 
