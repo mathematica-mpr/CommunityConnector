4f1171a4faa9d92a1229643cb9e9fd36cbc0f96b
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-10-31 14:05:35 -0400

    feat: use CV to pick mtry

--- methodology/codes/utilities.R
+++ methodology/codes/utilities.R
@@ -76,9 +76,11 @@ get.elbow.points.indices <- function(x, y, threshold) {
 }
 
 # TODO: option to remove modifiable, relevant SDoH scores or inputs to get a prediction. Similarity score = distance between predictions
-county_distance <- function(use_data, method, outcome, mtry = NULL){
+# TODO: ideally if we had more data, we would split the data into train and test sets to build the models
+county_distance <- function(use_data, method, outcome, show_deets = FALSE){
   mse <- NA
   formula <- as.formula(paste(outcome,"~."))
+  metric <- "RMSE"
   
   if(method == "euclidean"){
     use_data <- replace_nas_mean(use_data)
@@ -86,23 +88,24 @@ county_distance <- function(use_data, method, outcome, mtry = NULL){
                                   dplyr::select(-!!rlang::sym(outcome)),
                                 method = 'euclidean'))
   } else if(grepl("rf",method)){
+    meth <- "rf"
     use_data <- replace_nas_rf(use_data, outcome)
     set.seed(1234)
     
-    # TODO: optimize any parameters here? - tried optimizing mtry and ntree
+    # cross-validation on mtry
+    trControl <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
+    tunegrid <- expand.grid(.mtry=c(sqrt(ncol(use_data)):ncol(use_data)))
+    rf <- train(formula, data=use_data, method=meth, metric=metric, tuneGrid=tunegrid, trControl=trControl)
+    best_mtry <- rf$bestTune$mtry
+
+    rf <- randomForest(formula, data=use_data, method=meth, metric=metric, mtry = best_mtry,
+                       keep.forest = TRUE, importance = TRUE, proximity = TRUE)
     
-    trControl <- trainControl(method = 'cv', number = 10, search = 'grid')
-    rf <- randomForest(formula,
-                       data = use_data,
-                       method = 'rf',
-                       metric = 'RMSE',
-                       trControl = trControl, 
-                       keep.forest = TRUE,
-                       importance = TRUE,
-                       proximity = TRUE)
-    # importance(rf)
-    # varImp(rf, scale = FALSE)
-    # varImpPlot(rf, n.var=min(20,ncol(use_data)-1), sort = TRUE)
+    if(show_deets){
+      importance(rf)
+      varImp(rf, scale = FALSE)
+      varImpPlot(rf, n.var=min(20,ncol(use_data)-1), sort = TRUE)
+    }
     pred <- predict(rf)
     
     if(method == "rf proximity"){
@@ -190,4 +193,66 @@ evaluate_methodology <- function(data, use_outcome){
   pct_diff_from_county_med <- 100*abs(med_top5-this_county)/this_county
   pct_reduced_sd <- 100*abs(sd-sd_top5)/sd
   return(list(pct_diff_from_county_med, pct_reduced_sd))
+}
+
+implement_methodology <- function(row, outcomes, data, data_dictionary, num_counties = 1){
+  
+  # Define variables from opts dataframe
+  use_sdoh_scores <- as.numeric(row[1])
+  use_sdoh_raw <- as.numeric(row[2])
+  use_dems <- as.numeric(row[3])
+  methodology <- row[4]
+  meth_num <- as.numeric(row[5])
+  
+  print(methodology)
+  
+  # Loop through all outcomes
+  for(use_outcome in outcomes){
+    print(paste("Outcome:", use_outcome))
+    
+    orig_data <- data %>% 
+      filter(!is.na(!!rlang::sym(use_outcome)))
+    
+    # Select variables to match on, limit data to these variables, and replace NAs
+    use_data <- select_distance_columns(data = orig_data, data_dictionary = data_dictionary,
+                                        sdoh_scores = use_sdoh_scores, sdoh_raw = use_sdoh_raw,
+                                        outcome = use_outcome, dem = use_dems)
+    
+    # Get distance matrix using methodology specified
+    dist_results <- county_distance(use_data, methodology, use_outcome)
+    distancem <- dist_results[1][[1]]
+    mse <- dist_results[2][[1]]
+    
+    # num_counties <- dim(distancem)[1]
+    
+    # Loop through counties
+    for(county_num in c(1:num_counties)){
+      
+      data <- select_county(orig_data, distancem, county_num)
+      
+      ## Evaluate the methodology:
+      # Look at the radar charts in order of county similarity
+      # How similar are health outcomes of the top 5 most similar or similar within a certain distance?
+      # They should have a median close to the county in question
+      results <- evaluate_methodology(data, use_outcome)
+      results <- c(county_num, use_outcome, methodology, meth_num, results)
+      results_df = as.data.frame(matrix(unlist(results), nrow = 1))
+      colnames(results_df) <- c('county','outcome','methodology','meth_num','pct_diff_from_county_med','pct_reduced_sd')
+      num_vars <- c('county','meth_num','pct_diff_from_county_med','pct_reduced_sd')
+      results_df[,num_vars] <- as.numeric(as.character(unlist(results_df[,num_vars])))
+      results_df$mse <- mse
+      
+      # append results of all counties
+      if(use_outcome == outcomes[1]){
+        full_results <- results_df
+      } else {
+        full_results <- full_results %>% 
+          rbind(results_df)
+      }
+      
+    }
+  }
+  
+  print(full_results)
+  return(full_results)
 }
