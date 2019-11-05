6f7906b53725518c2c0057c0f6a97504e69b3975
Margaret Luo <MLuo@mathematica-mpr.com>
2019-11-05 18:08:21 -0500

    feat: placeholder header for map

--- DockerShinyApp/app/ui.R
+++ DockerShinyApp/app/ui.R
@@ -33,7 +33,8 @@ ui <- fluidPage(#theme = "styles.css",
   
   fluidRow(
     column(width = 4,
-           plotlyOutput("map")),
+           fluidRow(uiOutput('map_header')),
+           fluidRow(plotlyOutput("map"))),
     column(width = 4, 
            fluidRow(uiOutput('my_matches_header')),
            fluidRow(plotOutput("compare_county_radars"))),
