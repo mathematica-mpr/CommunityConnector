ffc59b19db98aaa8084bf9324f4be1c98d454912
Margaret Luo <MLuo@mathematica-mpr.com>
2019-11-08 10:44:35 -0500

    feat: migrate plotly radar functions into functions

--- DockerShinyApp/app/r/functions.R
+++ DockerShinyApp/app/r/functions.R
@@ -73,6 +73,171 @@ arrange_rank <- function(data, outcome_sort) {
   
 } 
 
+# function for one county radar plot -------------------------------------------
+radar_chart <- function(df, dictionary) {
+  # df is my county; columns: county, state, sdoh_score 1:6
+  # dictionary is data dictionary
+  
+  #vector of score names
+  radar_names <- get_dd(dictionary, "sdoh_score") %>% 
+    dplyr::pull(3)
+  radar_names <- append(radar_names, radar_names[1])
+  #vector of score values
+  radar_points <- select(df, starts_with("sdoh"))
+  radar_points <- append(radar_points, radar_points[1]) %>% 
+    unlist()
+  
+  #plotting radar chart
+  p <- plot_ly(
+  ) %>% 
+    add_trace(
+      type = 'scatterpolar',
+      mode = 'markers+lines',
+      r = radar_points,
+      theta = radar_names,
+      #aesthetics
+      fill = 'toself',
+      fillcolor = paste0(config$colors$yellow50),
+      line = list(dash = "solid", 
+                  color = paste0(config$colors$red100), 
+                  width = .8, 
+                  shape = 'spline', 
+                  smoothing = .9),
+      marker = list(size = 7,
+                    color = paste0(config$colors$red100),
+                    opacity = 1),
+      opacity = .9,
+      #hover label
+      name = paste(df$county, df$state),
+      hovertemplate = ~paste('<b>Category</b>: %{theta}',
+                             '<br><b>Score</b>: %{r:.2f}',
+                             '<extra></extra>')
+    ) %>% 
+    layout(
+      polar = list(
+        #tick labels
+        radialaxis = list(
+          range = c(0,1),
+          angle = 90,
+          tickangle = 90,
+          dtick = .5, 
+          tickwidth = 2,
+          tickfont = list(size = 10)
+        ),
+        #category labels
+        angularaxis = list(
+          tickfont = list(size =  12),
+          rotation = 0
+        ),
+        bgcolor = paste0(config$colors$tan25, '50')
+      ),
+      #hover label aesthetics
+      hoverlabel = list(
+        bordercolor = paste0(config$colors$black, '100'),
+        bgcolor = paste0(config$colors$red50)
+      ),
+      margin = list(t=70)
+    )
+  return(p)
+}
+
+# function for two county radar plot -------------------------------------------
+radar_chart_overlay <- function(df1, df2, dictionary) {
+  # df1 is my county; columns: county, state, sdoh_score 1:6
+  # df2 is comparison county, same columns as df1
+  # dictionary is data dictionary
+  
+  #vector of score names
+  radar_names <- get_dd(dictionary, "sdoh_score") %>% 
+    dplyr::pull(3)
+  radar_names <- append(radar_names, radar_names[1])
+  #vector of score values
+  radar_points1 <- select(df1, starts_with("sdoh"))
+  radar_points1 <- append(radar_points1, radar_points1[1]) %>% 
+    unlist()
+  radar_points2 <- select(df2, starts_with("sdoh"))
+  radar_points2 <- append(radar_points2, radar_points2[1]) %>% 
+    unlist()
+  
+  #plotting radar chart
+  p <- plot_ly(
+  ) %>%     
+    add_trace(
+      type = 'scatterpolar',
+      mode = 'markers+lines',
+      r = radar_points2,
+      theta = radar_names,
+      fill = "toself",
+      fillcolor = paste0(config$colors$teal100, "CC"),
+      line = list(dash = "solid", 
+                  color = paste0(config$colors$green100), 
+                  width = .8, 
+                  shape = 'spline', 
+                  smoothing = .9),
+      marker = list(size = 7,
+                    color = paste0(config$colors$green100),
+                    opacity = 1),
+      opacity = .6,
+      hoverinfo = 'none',
+      name = paste(df2[1,1], "County")
+    ) %>% 
+    add_trace(
+      type = 'scatterpolar',
+      mode = 'markers+lines',
+      r = radar_points1,
+      theta = radar_names,
+      #aesthetics
+      fill = 'toself',
+      fillcolor = paste0(config$colors$yellow50, "CC"),
+      line = list(dash = "solid", 
+                  color = paste0(config$colors$red100), 
+                  width = .8, 
+                  shape = 'spline', 
+                  smoothing = .9),
+      marker = list(size = 7,
+                    color = paste0(config$colors$red100),
+                    opacity = 1),
+      opacity = .9,
+      #hover label
+      hoverinfo = 'none',
+      name = paste(df1[1,1], "County")
+    ) %>% 
+    layout(
+      title = list(
+        text = paste(df2[1,1], "County,", df2[1,2]),
+        font = list(
+          size = 18
+        ),
+        xref = 'paper'
+      ),
+      polar = list(
+        #tick labels
+        radialaxis = list(
+          range = c(0,1),
+          angle = 90,
+          tickangle = 90,
+          dtick = .5, 
+          tickwidth = 2,
+          tickfont = list(size = 10)
+        ),
+        #category labels
+        angularaxis = list(
+          tickfont = list(size =  12),
+          rotation = 0
+        ),
+        bgcolor = paste0(config$colors$tan25, '50')
+      ),
+      #hover label aesthetics
+      hoverlabel = list(
+        bordercolor = paste0(config$colors$black, '100'),
+        bgcolor = paste0(config$colors$red50)
+      ),
+      margin = list(t=70),
+      showlegend = T
+    )
+  return(p)
+}
+
 make_radar_data <- function(county_df, dd) {
   df <- rbind(rep(1, 6), rep(0, 6),
               # 50% circle color
