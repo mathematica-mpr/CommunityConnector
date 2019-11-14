738b1de8a5fb4936644aefffd7c24d8a564e27d7
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-11-14 12:35:25 -0500

    fix: percentages

--- methodology/codes/00_select_variables.py
+++ methodology/codes/00_select_variables.py
@@ -5,7 +5,7 @@ pd.set_option('display.max_columns', 30)
 import numpy as np
 import sys
 sys.path.insert(1, 'pipeline/codes')
-from utilities import available_vars
+from utilities import available_vars, custom_replace, fix_percentages, check_negatives
 
 datafile = "data/full_data.csv"
 data = pd.read_csv(datafile)
@@ -18,14 +18,12 @@ data_dictionary = pd.read_csv('data/data_dictionary.csv')
 data_dictionary.columns = map(str.lower, data_dictionary.columns)
 keep_cols = data_dictionary['column_name']
 keep_cols = [col for col in keep_cols if 'sdoh_score' not in col]
-data['med_2br_rent_per_med_inc'] = data['rent_twobed2015']/(data['median_income']/12)
+data['med_2br_rent_per_med_inc'] = 100*data['rent_twobed2015']/(data['median_income']/12)
 data['state'] = "CO"
 
 print("Number of columns from data dictionary: " + str(len(keep_cols)))
 
 # doing this because I messed up the data dictionary - not sure if this is needed anymore
-def custom_replace(col):
-    return col.replace("% ","pct_").replace("< ","lt_").replace("/","_").replace("%","pct").replace(" ", "_").replace("(","").replace(")","").replace("-","").replace("__","_")
 data.columns = [custom_replace(col) for col in data.columns.values]
 
 data = data[keep_cols]
@@ -43,6 +41,12 @@ for var in data.columns.values:
 print("# columns dropped due to low coverage: " + str(count))
 
 # check to make sure all of the percentage variables are between 0-100, not 0-1
+print(data_dictionary.data_type.drop_duplicates())
 
+# checked rates too, none of those need adjusting as of now
+data = fix_percentages(data_dictionary, data)
+
+check_negatives(data_dictionary, data)
+data.loc[data['budget_health_info'] < 0,'budget_health_info'] = 0
 
 data.to_csv('data/full_data_relative.csv', index = False)
