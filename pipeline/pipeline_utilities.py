2715791e51c0462683f7ffe5a2ed49b561c63751
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-11-17 12:51:27 -0500

    feat: merge data

--- pipeline/pipeline_utilities.py
+++ pipeline/pipeline_utilities.py
@@ -104,3 +104,52 @@ def OppAtlas(output, input = 'data/raw/opp_atlas_stay.csv',
 
     print(output)
     data.to_csv(output, index = False)
+
+def MergeCleaned(cleaned_drive, outdir, data_types = ['01_Demographic','02_SDoH','03_Outcome']):
+
+    count = 0
+    for t in data_types:
+        cleaned_files = os.listdir(os.path.join(cleaned_drive, t))
+
+        for file in cleaned_files:
+            if "csv" in file:
+                data = pd.read_csv(os.path.join(cleaned_drive, t, file))
+                print(file)
+                print(data.shape)
+
+                data['FIPS'] = [int(str(fips)[-3:]) for fips in data['FIPS']]
+                # make sure the file is unique by FIPS
+                print(data['FIPS'].drop_duplicates().shape)
+                data_columns = pd.DataFrame({'column_name': data.columns.values,
+                    'type': t
+                    })
+
+                if count == 0:
+                    full_data = data
+                    columns = data_columns
+                else:
+                    full_data = pd.merge(full_data, data, on = 'FIPS', how = 'outer')
+                    columns = columns.append(data_columns)
+                print(f"Completed {file}")
+                print(full_data.shape)
+                print('')
+
+                count += 1
+
+    # now that we've merged on FIPS, add the 08 at the beginning for Colorado state code
+    full_data['FIPS'] = ["08"+str(fips).zfill(3) for fips in full_data['FIPS']]
+    # drop counties that have FIPS = 999 or 0
+    full_data = full_data[full_data['FIPS'] != "08999"]
+    full_data = full_data[full_data['FIPS'] != "08000"]
+    full_data['County'] = [col + " County" for col in full_data['County']]
+
+    print(full_data.shape)
+    # Custom check for Colorado
+    assert(full_data.shape[0] == 64)
+
+    # add sdoh_score1-6 to columns
+    sdoh_score_names = [f'sdoh_score{i}' for i in range(1,7)]
+    columns = columns.append(pd.DataFrame({'column_name': sdoh_score_names,
+    'type': ['aggregate']*6}))
+
+    full_data.to_csv(os.path.join(outdir, 'full_data.csv'), index = False)
