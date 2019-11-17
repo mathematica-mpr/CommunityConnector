7f3bcd26ad860b84628f568f45a5bd862f3774f6
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-11-17 13:22:46 -0500

    refactor: SelectVariables

--- pipeline/pipeline_utilities.py
+++ pipeline/pipeline_utilities.py
@@ -17,9 +17,14 @@ options.add_argument('--blink-settings=imagesEnabled=false')
 
 import time
 import pandas as pd
+# view non-truncated dataframe
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', 30)
 
 from io import StringIO
 import requests
+
+import numpy as np
 
 def click_button(driver, element_type, element_name, time_to_wait = 10):
     """
@@ -153,3 +158,74 @@ def MergeCleaned(cleaned_drive, outdir, data_types = ['01_Demographic','02_SDoH'
     'type': ['aggregate']*6}))
 
     full_data.to_csv(os.path.join(outdir, 'full_data.csv'), index = False)
+
+def custom_replace(col):
+    return col.replace("% ","pct_").replace("< ","lt_").replace("/","_").replace("%","pct").replace(" ", "_").replace("(","").replace(")","").replace("-","").replace("__","_")
+
+def fix_percentages(data_dictionary, data):
+
+    pct_vars = data_dictionary[data_dictionary['data_type'] == 'percentage']['column_name']
+    for col in pct_vars:
+        if max(data[col]) < 1:
+            print(col + " is being adjusted")
+            print(max(data[col]))
+            print(min(data[col]))
+            data[col] = data[col] * 100
+    return data
+
+def check_negatives(data_dictionary, data):
+    print("Any negative values in data?")
+    for col in data_dictionary[data_dictionary['data_type'] != 'ID']['column_name']:
+        if 'sdoh_score' not in col:
+            if min(data[col]) < 0:
+                print(col)
+
+def check_low_coverage(data):
+    nrows = data.shape[0]
+    count = 0
+    for var in data.columns.values:
+        coverage = data[pd.notnull(data[var])].shape[0]/nrows
+        # TODO: this is an arbitrary cutoff
+        if coverage < 0.8:
+            print(f"Dropping {var} due to low coverage ({round(coverage,2)*100}%)")
+            data.drop([var], axis = 1, inplace = True)
+            count += 1
+    return data, count
+
+def SelectVariables(input, output, data_dictionary = 'data/data_dictionary.csv'):
+
+    data = pd.read_csv(input)
+    print("Original shape of data: " + str(data.shape))
+    data.columns = map(str.lower, data.columns)
+
+    # keep variables from data dictionary - mostly created in cleaning codes
+    data_dictionary = pd.read_csv(data_dictionary)
+    data_dictionary.columns = map(str.lower, data_dictionary.columns)
+    keep_cols = data_dictionary['column_name']
+    keep_cols = [col for col in keep_cols if 'sdoh_score' not in col]
+    # TODO: move to earlier cleaning
+    data['med_2br_rent_per_med_inc'] = 100*data['rent_twobed2015']/(data['median_income']/12)
+    data['state'] = "CO"
+
+    print("Number of columns from data dictionary: " + str(len(keep_cols)))
+
+    # doing this because I messed up the data dictionary - not sure if this is needed anymore
+    data.columns = [custom_replace(col) for col in data.columns.values]
+
+    data = data[keep_cols]
+    assert(data.shape[1] == len(keep_cols))
+
+    # are there any variables that don't have a ton of data? drop them if low coverage
+    data, count = check_low_coverage(data)
+    print("# columns dropped due to low coverage: " + str(count))
+
+    # check to make sure all of the percentage variables are between 0-100, not 0-1
+    print(data_dictionary.data_type.drop_duplicates())
+
+    # checked rates too, none of those need adjusting as of now
+    data = fix_percentages(data_dictionary, data)
+
+    check_negatives(data_dictionary, data)
+    data.loc[data['budget_health_info'] < 0,'budget_health_info'] = 0
+
+    data.to_csv(output, index = False)
