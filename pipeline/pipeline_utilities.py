f6efb275bed58892e37f51b3b02c733bf120cfdb
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-11-17 12:38:02 -0500

    feat: pipeline using DemOppAtlas

---
+++ pipeline/pipeline_utilities.py
@@ -0,0 +1,106 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+import regex as re
+import os
+from stat import S_ISREG, ST_CTIME, ST_MODE, ST_MTIME
+import shutil
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument('--headless')
+# options to try to improve performance
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+options.add_argument('--blink-settings=imagesEnabled=false')
+
+import time
+import pandas as pd
+
+from io import StringIO
+import requests
+
+def click_button(driver, element_type, element_name, time_to_wait = 10):
+    """
+    Wait for element to be clickable, then find and click on the element
+
+    Args: 
+        driver
+        element_type (string): class, ID, or text
+        element_name (string)
+        time_to_wait (integer)
+
+    """
+
+    if element_type == "class":
+        placeholder = WebDriverWait(driver, time_to_wait).until(EC.element_to_be_clickable((By.CLASS_NAME, element_name)))
+        button = driver.find_element_by_class_name(element_name)
+    elif element_type == "ID":
+        placeholder = WebDriverWait(driver, time_to_wait).until(EC.element_to_be_clickable((By.ID, element_name)))
+        button = driver.find_element_by_id(element_name)
+    elif element_type == "text":
+        placeholder = WebDriverWait(driver, time_to_wait).until(EC.element_to_be_clickable((By.LINK_TEXT, element_name)))
+        button = driver.find_element_by_link_text(element_name)
+    button.click()
+
+def move_from_downloads(orig_path, search_term, new_path, new_name):
+    files = os.listdir(orig_path)
+    files = [x for x in files if re.search(search_term, x)] 
+    entries = (os.path.join(orig_path, fn) for fn in os.listdir(orig_path))
+    entries = ((os.stat(path), path) for path in entries)
+    # leave only regular files, insert creation date
+    #NOTE: on Windows `ST_CTIME` is a creation date 
+    #NOTE: use `ST_MTIME` to sort by a modification date
+    entries = ((stat[ST_MTIME], path)
+               for stat, path in entries if S_ISREG(stat[ST_MODE]))
+    count = 0
+    for cdate, path in sorted(entries, reverse=True):
+        # keep only the first, most recent file name
+        while count == 0:
+            filename = os.path.basename(path)
+            count += 1
+            print(filename)
+
+    shutil.move(orig_path + filename, os.path.join(new_path, new_name))
+
+# TODO: fix selenium.common.exceptions.SessionNotCreatedException: Message: session not created: This version
+# of ChromeDriver only supports Chrome version 76
+# https://stackoverflow.com/questions/57600228/sessionnotcreatedexception-message-session-not-created-this-version-of-chrome
+
+def GeographicPUF(outdir, downloads = 'C:/Users/kskvoretz/Downloads/',
+    website = "https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Geographic-Variation/GV_PUF.html"):
+
+    driver = webdriver.Chrome(options=options)
+    driver.delete_all_cookies()
+    driver.get(website)
+
+    click_button(driver, "text","State/County Table - All Beneficiaries [ZIP, 68MB]")
+    time.sleep(300)
+    move_from_downloads(downloads, "State-County-All-Table-2017",
+    outdir, "geo_var_data.zip")
+
+def OppAtlas(output, input = 'data/raw/opp_atlas_stay.csv',
+    url="https://opportunityinsights.org/wp-content/uploads/2018/12/cty_covariates.csv"):
+    
+    s=requests.get(url).text
+
+    data = pd.read_csv(StringIO(s), sep=",")
+    data = data[['state','county','frac_coll_plus2010','rent_twobed2015','traveltime15_2010','ann_avg_job_growth_2004_2013']]
+    data = data[data['state'] == 8]
+    data.drop(['state'], axis = 1, inplace = True)
+    data.columns.values[0] = "FIPS"
+
+    # TODO: need to scrape this, but pulled data for now using the following selections:
+    # Download the Data --> % Staying in Same Tract as Adults --> Counties
+    # "All" in the subgroup selections below
+    stay = pd.read_csv(input, encoding = "ISO-8859-1")
+    stay['State'] = [re.split(', ',state,1)[1] for state in stay['Name']]
+    stay['FIPS'] = [int(cty[-3:]) for cty in stay['cty']]
+    stay = stay[stay['State'] == "CO"]
+    stay = stay[['%_Staying_in_Same_Tract_as_Adults_rP_gP_pall','FIPS']]
+
+    data = pd.merge(data, stay, on = "FIPS")
+
+    print(output)
+    data.to_csv(output, index = False)
