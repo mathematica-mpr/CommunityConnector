638cc6a41d14775414ae6cf8b266152401d527f6
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-01 14:15:58 -0500

    refactor: keep variables throughout exploring variables

--- pipeline/scraping_codes/01_Demographic/00_acs_data.py
+++ pipeline/scraping_codes/01_Demographic/00_acs_data.py
@@ -33,7 +33,7 @@ print(use_vars[['TableID','Stub','Use']])
 use_vars.to_csv(os.path.join(n_drive, 'ACS_variables.csv'))
 variables = use_vars.TableID.tolist()
 
-# # Method 2: Use the census data package
+# Use the census data package
 # Examples of functionality
 censusdata.search('acs5', 2017, 'label','unemploy')
 # censusdata.search('acs5', 2017, 'concept', 'education')
@@ -64,14 +64,17 @@ print(full_data['FIPS'])
 full_data.drop(columns = "location", inplace = True)
 
 full_data.columns = map(str.lower, full_data.columns)
-# columns to keep
+
+# determine which columns to keep
 print(full_data.columns.values)
+keep_vars = ['fips']
 
 race_cols = available_vars(full_data, 'race')
 drop_race_cols = ["race_estimate_total", "race_estimate_total_two_or_more_races_two_races_including_some_other_race",
 "race_estimate_total_two_or_more_races_two_races_excluding_some_other_race_and_three_or_more_races"]
 race_cols = np.setdiff1d(race_cols, drop_race_cols)
 full_data[race_cols] = full_data[race_cols].apply(lambda x: x/full_data['race_estimate_total'])
+keep_vars.extend(race_cols)
 
 eng_vars = available_vars(full_data, 'english')
 eng_vars = ['place_of_birth_by_language_spoken_at_home_and_ability_to_speak_english_in_the_united_states_estimate_total_speak_only_english',
@@ -80,23 +83,19 @@ eng_vars = ['place_of_birth_by_language_spoken_at_home_and_ability_to_speak_engl
 full_data[eng_vars] = full_data[eng_vars].apply(lambda x: x/full_data['place_of_birth_by_language_spoken_at_home_and_ability_to_speak_english_in_the_united_states_estimate_total'])
 new_eng_vars = ['pct_only_english','pct_other_languages','pct_other_languages_eng_vwell']
 full_data[new_eng_vars] = full_data[eng_vars]
+keep_vars.extend(new_eng_vars)
 
 available_vars(full_data, 'occupancy')
 full_data['pct_vacant'] = full_data['occupancy_status_estimate_total_vacant']/full_data['occupancy_status_estimate_total']
+keep_vars.append('pct_vacant')
 
 available_vars(full_data, 'median_value')
 full_data['median_value'] = full_data['median_value_(dollars)_estimate_median_value_(dollars)']
-
 full_data['median_income'] = full_data['median_household_income_in_the_past_12_months_(in_2017_inflation-adjusted_dollars)_estimate_median_household_income_in_the_past_12_months_(in_2017_inflation-adjusted_dollars)']
 full_data['mean_hours'] = full_data['mean_usual_hours_worked_in_the_past_12_months_for_workers_16_to_64_years_estimate_total']
-keep_vars = ['fips']
-keep_vars.extend(race_cols)
-keep_vars.extend(new_eng_vars)
 keep_vars.extend(['median_income',
 'gini_index_of_income_inequality_estimate_gini_index',
-'mean_hours'])
-keep_vars.append('pct_vacant')
-keep_vars.append('median_value')
+'mean_hours','median_value'])
 
 full_data = full_data[keep_vars]
 full_data.columns.values[0] = 'FIPS'
