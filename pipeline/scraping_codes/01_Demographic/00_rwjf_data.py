319af5f2b511a1a14d071f714c761bc45e7067fa
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-01 14:52:48 -0500

    refactor: cleaning rwjf functions

--- pipeline/scraping_codes/01_Demographic/00_rwjf_data.py
+++ pipeline/scraping_codes/01_Demographic/00_rwjf_data.py
@@ -4,6 +4,9 @@ from urllib.parse import urljoin
 import gzip
 import pandas as pd
 import numpy as np
+import sys
+sys.insert('pipeline/scraping_codes')
+from utilities import rwjf_concatenate_column_names, rwjf_replace_column_names
 
 states = {"CO":"Colorado"}
 
@@ -31,42 +34,19 @@ for lkp in states:
 
 print(read_state)
 
-read_state = 'Colorado'
 n_drive = 'N:/Transfer/KSkvoretz/AHRQ/data/01_Demographic/RWJF'
 output = 'data/cleaned/01_Demographic'
 
-def replace_column_names(df):
-    colnames = df.head(1).values
-    colnames = list(colnames)[0]
-    df.columns = colnames
-    
-    # drop the first row that contains the column names
-    df.drop(df.index[0], inplace = True)
-    return df
-
-def concatenate_column_names(df):
-    newcols = []
-    for col in df.columns.values:
-        if col in [0, 'FIPS','State','County']:
-            newcols.append(col)
-        else:
-            if col not in ['95% CI - Low','95% CI - High','Z-Score']:
-                metric = col
-                newcols.append(col)
-            else:
-                newcols.append(metric + "_" + col)
-    return newcols
-
-measure_data = pd.read_csv(os.path.join(n_drive, f'Measure_Data_{read_state}.csv'))
-measure_data = replace_column_names(measure_data)
-measure_data.columns = concatenate_column_names(measure_data)
+measure_data = rwjf_replace_column_names(measure_data)
+measure_data.columns = rwjf_concatenate_column_names(measure_data)
 measure_data.drop(['Population',0], axis = 1, inplace = True)
 measure_data.rename(columns = {"% Uninsured": "pct_uninsured"}, inplace = True)
 
-addtl_data = pd.read_csv(os.path.join(n_drive, f'Additional_Data{read_state}.csv'))
-addtl_data = replace_column_names(addtl_data)
-addtl_data.columns = concatenate_column_names(addtl_data)
+addtl_data = rwjf_replace_column_names(addtl_data)
+addtl_data.columns = rwjf_concatenate_column_names(addtl_data)
 addtl_data.drop([0], axis = 1, inplace = True)
+
+# there are two columns called % uninsured
 ind = 0
 first = 0
 for col in addtl_data.columns.values:
