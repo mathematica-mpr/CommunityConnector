a187bd3a1b12463b9c8c519a654687453f593a21
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-01 17:15:34 -0500

    refactor: one code for cdphe

--- pipeline/scraping_codes/03_Outcome/00_cdphe_diab_hosp_over.py
+++ pipeline/scraping_codes/03_Outcome/00_cdphe_diab_hosp_over.py
@@ -58,3 +58,15 @@ print(data.head())
 print(data.shape)
 
 data.to_csv('data/raw/cdphe.csv', index = False)
+
+import pandas as pd 
+import sys
+sys.path.insert(1, 'pipeline/codes')
+from utilities import remove_from_dict
+
+data = pd.read_csv('data/raw/cdphe.csv')
+data['overobese_pct'] = data['overobese_pct']/100
+print(data.head())
+print(data.shape)
+
+data.to_csv('data/cleaned/03_Outcome/cdphe_cleaned.csv', index = False)
