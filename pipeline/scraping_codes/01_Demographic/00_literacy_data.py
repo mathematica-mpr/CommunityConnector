18e4150600807e77f46ccda8a12a4d3d8635d604
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-01 14:28:51 -0500

    fix: remove /100 for percent

--- pipeline/scraping_codes/01_Demographic/00_literacy_data.py
+++ pipeline/scraping_codes/01_Demographic/00_literacy_data.py
@@ -9,7 +9,7 @@ from utilities import remove_from_dict
 data = pd.read_csv('data/raw/literacy_raw.csv')
 data = data[data['FIPS'] != 8000]
 data = data[['FIPS','% Lacking basic literacy']]
-data['pct_lack_basic_literacy'] = data['% Lacking basic literacy']/100
+data['pct_lack_basic_literacy'] = data['% Lacking basic literacy']
 data.drop(['% Lacking basic literacy'], axis = 1, inplace = True)
 print(data.head())
 print(data.shape)
