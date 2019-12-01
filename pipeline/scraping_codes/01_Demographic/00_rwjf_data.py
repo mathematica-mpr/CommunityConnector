a4af7ef4443c945e2bc453a3056da33a4b1033ee
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-01 14:43:09 -0500

    feat: pull only colorado

--- pipeline/scraping_codes/01_Demographic/00_rwjf_data.py
+++ pipeline/scraping_codes/01_Demographic/00_rwjf_data.py
@@ -11,7 +11,7 @@ import pandas as pd
 # #### 4. Additional Measure Data - yes, raw data
 # #### 5./6. tabs on sources for reference: Ranked Measure Sources & Years and Addtl Measure Sources & Years
 
-states = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
+states = {"CO":"Colorado"}
 
 def file_template(state, version):
     file = f'https://www.countyhealthrankings.org/sites/default/files/state/downloads/2019%20County%20Health%20Rankings%20{state}%20Data%20-%20v{version}.xls'
