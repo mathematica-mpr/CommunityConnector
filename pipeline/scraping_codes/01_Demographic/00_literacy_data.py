1287f17de1f42d2b0c7dde58323411714cb4fac3
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-01 14:29:19 -0500

    fix: remove addition to dictionary

--- pipeline/scraping_codes/01_Demographic/00_literacy_data.py
+++ pipeline/scraping_codes/01_Demographic/00_literacy_data.py
@@ -19,23 +19,4 @@ data_dict = results[0]
 add_cols = results[1]
 print(add_cols)
 
-add_rows = pd.DataFrame({'column_name': add_cols,
-'description': ['Those lacking Basic prose literacy skills include those who scored Below Basic in prose and those who could not be tested due to language barriers.'],
-'demographic': [1],
-'sdoh_raw': [0],
-'outcome': [0],
-'sdoh_score': [0],
-'data_type': ['percentage'],
-'used_sdoh_1': [0],
-'used_sdoh_2': [0],
-'used_sdoh_3': [0],
-'used_sdoh_4': [0],
-'used_sdoh_5': [0],
-'used_sdoh_6': [0],
-'source': ['NCES']
-})
-data_dict = data_dict.append(add_rows)
-print(data_dict)
-data_dict.to_csv('data/data_dictionary.csv', index = False)
-
 data.to_csv('data/cleaned/01_Demographic/literacy_cleaned.csv', index = False)
