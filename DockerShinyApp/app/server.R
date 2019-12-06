cac4ca71ae51809309b251fe7f95504f916918b4
Evelyn Cody <ECody@mathematica-mpr.com>
2019-12-06 12:26:32 -0600

    fix: retitled checkbox group

--- DockerShinyApp/app/server.R
+++ DockerShinyApp/app/server.R
@@ -234,7 +234,7 @@ server <- function(input, output, session) {
     tagList(
       fluidRow(
         column(width = 12, 
-               checkboxGroupInput('demo_filter', label = 'Add or filter by categories:', 
+               checkboxGroupInput('demo_filter', label = 'Filter by categories:', 
                                       choices = c('Economic Stability' = 'used_sdoh_1',
                                                   'Neighborhood & Physical Environment' = 'used_sdoh_2',
                                                   'Education' = 'used_sdoh_3',
