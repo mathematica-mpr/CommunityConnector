82c4a5babcb79a6119909de88611d644341ae645
Kelsey Skvoretz <KSkvoretz@mathematica-mpr.com>
2019-12-10 14:19:51 -0500

    style: clean up

--- pipeline/scraping_codes/03_Outcome/00_cdc_diabetes_surveillance.R
+++ pipeline/scraping_codes/03_Outcome/00_cdc_diabetes_surveillance.R
@@ -20,31 +20,27 @@ diab_prev_2016 <- process_atlas("DiabetesAtlasCountyData_Prev2016.csv", "diabete
 diab_inc_2011 <- process_atlas("DiabetesAtlasCountyData_Inc2011.csv", "diabetes_incidence_2011")
 diab_inc_2016 <- process_atlas("DiabetesAtlasCountyData_Inc2016.csv", "diabetes_incidence_2016")
 obese_prev_2011 <- process_atlas("DiabetesAtlasCountyData_Obes2011.csv", "obesity_prevalence_2011")
-
-obese_prev_2016 <- read.csv("DiabetesAtlasCountyData_Obes2016.csv", header=FALSE)
-
-
-obese_prev_2016=obese_prev_2016[-c(1,2,3,68),]
-obese_prev_2016=obese_prev_2016[,-c(2,5,6)]
-
-names(obese_prev_2016)<-c("county", "FIPS","obesity_prevalence_2016")
+obese_prev_2016 <- process_atlas("DiabetesAtlasCountyData_Obes2016.csv", "obesity_prevalence_2016")
 
 colorado<-merge(diab_inc_2011, diab_inc_2016, by=c("county", "FIPS"), all.x = T, all.y = T)
 colorado<-merge(colorado, diab_prev_2011, by=c("county", "FIPS"), all.x = T, all.y = T)
 colorado<-merge(colorado, diab_prev_2016, by=c("county", "FIPS"), all.x = T, all.y = T)
 colorado<-merge(colorado, obese_prev_2011, by=c("county", "FIPS"), all.x = T, all.y = T)
 colorado<-merge(colorado, obese_prev_2016, by=c("county", "FIPS"), all.x = T, all.y = T)
+nrow(colorado)
 
-colorado[,2:8] <- sapply(colorado[,2:8],as.character)
-colorado[,2:8] <- sapply(colorado[,2:8],as.numeric)
+head(colorado)
+summary(colorado)
 
 colorado$diabetes_incidence_5_year_diff<-colorado$diabetes_incidence_2016-colorado$diabetes_incidence_2011
 colorado$diabetes_prevalence_5_year_diff<-colorado$diabetes_prevalence_2016-colorado$diabetes_prevalence_2011
 colorado$obesity_prevalence_5_year_diff<-colorado$obesity_prevalence_2016-colorado$obesity_prevalence_2011
+head(colorado)
 
 ## keep only 2016 and 5 year difference
-colorado<-colorado[,c(1,2,4,6,8:11)]
+colorado <- colorado %>% 
+  select(-ends_with("2011"))
+head(colorado)
 
 setwd('../cleaned/')
 write.csv(colorado, "CDC_Diabetes_Outcome_Data.csv", row.names=F)
-
